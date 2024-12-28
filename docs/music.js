8c542cb3220689d249276fba33dbdd94e0158f51
diewland <diewland@gmail.com>
2024-12-28 15:42:07 +0700

    add music config

---
+++ docs/music.js
@@ -0,0 +1,24 @@
+let playlist = {
+  host: "https://avawalk.github.io/avanut/mp3/",
+  edm: [
+    "AVA_Edm_Mode01.mp3",
+    "AVA_Edm_Mode02.mp3",
+    "AVA_Edm_Mode03.mp3",
+    "AVA_Edm_Mode04.mp3",
+    "AVA_Edm_Mode05.mp3",
+  ],
+  fight: [
+    "AVA_Fight_Mode01.mp3",
+    "AVA_Fight_Mode02.mp3",
+    "AVA_Fight_Mode03.mp3",
+    "AVA_Fight_Mode04.mp3",
+    "AVA_Fight_Mode05.mp3",
+  ],
+  snow: [
+    "AVA_Snow_Mode01.mp3",
+    "AVA_Snow_Mode02.mp3",
+    "AVA_Snow_Mode03.mp3",
+    "AVA_Snow_Mode04.mp3",
+    "AVA_Snow_Mode05.mp3",
+  ],
+}
